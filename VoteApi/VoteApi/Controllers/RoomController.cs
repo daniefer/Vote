8deecaaa561c0c4373cfcd32cdfe560b0f900c03
Dan Ferguson <daniefer@icloud.com>
2018-12-05 19:53:38 -0600

    Add a get all rooms endpoint

--- VoteApi/VoteApi/Controllers/RoomController.cs
+++ VoteApi/VoteApi/Controllers/RoomController.cs
@@ -10,33 +10,39 @@ using VoteApi.Models;
 
 namespace VoteApi.Controllers
 {
-    [Route(_ClassRoute)]
-    [ApiController]
-    public class RoomController : ControllerBase
-    {
-        private const string _ClassRoute = "api/" + Constants.RoomsRoute;
-        private const string _ParticipantsByRoomRouteParameter = "{roomId}/" + Constants.ParticipantsRoute;
+	[Route(_ClassRoute)]
+	[ApiController]
+	public class RoomController : ControllerBase
+	{
+		private const string _ClassRoute = "api/" + Constants.RoomsRoute;
+		private const string _ParticipantsByRoomRouteParameter = "{roomId}/" + Constants.ParticipantsRoute;
 
-        private readonly ApiContext _context;
+		private readonly ApiContext _context;
 
-        public RoomController(ApiContext context)
-        {
-            _context = context;
-        }
+		public RoomController(ApiContext context)
+		{
+			_context = context;
+		}
 
-        [HttpPost]
-        public async Task<ActionResult<Room>> PostAsync([FromBody] Room room, CancellationToken cancellationToken)
-        {
-            var result = _context.Rooms.Add(room);
-            await _context.SaveChangesAsync(cancellationToken);
-            return result.Entity;
-        }
+		[HttpGet]
+		public async Task<ActionResult<List<Room>>> GetAsync(CancellationToken cancellationToken)
+		{
+			return Ok(await _context.Rooms.AsNoTracking().ToListAsync(cancellationToken));
+		}
 
-        [HttpGet(_ParticipantsByRoomRouteParameter)]
-        public async Task<ActionResult<List<Participant>>> GetAllAsync([FromRoute]int roomId, CancellationToken cancellationToken)
-        {
-            var participants = await _context.Participants.AsNoTracking().Where(Participant.GetByRoomId(roomId)).ToListAsync(cancellationToken);
-            return participants;
-        }
-    }
+		[HttpPost]
+		public async Task<ActionResult<Room>> PostAsync([FromBody] Room room, CancellationToken cancellationToken)
+		{
+			var result = _context.Rooms.Add(room);
+			await _context.SaveChangesAsync(cancellationToken);
+			return result.Entity;
+		}
+
+		[HttpGet(_ParticipantsByRoomRouteParameter)]
+		public async Task<ActionResult<List<Participant>>> GetAllAsync([FromRoute]int roomId, CancellationToken cancellationToken)
+		{
+			var participants = await _context.Participants.AsNoTracking().Where(Participant.GetByRoomId(roomId)).ToListAsync(cancellationToken);
+			return participants;
+		}
+	}
 }
